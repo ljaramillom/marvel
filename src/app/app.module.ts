c6f20fe1e8e69cbbaa9c63dcdeb84d90ab1db305
ljaramillom <jaramillo.lizet@gmail.com>
2020-10-29 20:53:38 -0500

    add files of config, components, icons, bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { CharactersComponent } from './components/characters/characters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FavoritesComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
